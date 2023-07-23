b5beca99b53c8049b7e4f0fd6ce943699f478912
Anh Le <anhld@users.noreply.github.com>
2023-07-24 06:39:04 +0700

    Make the field working with TreeDropdownField

--- client/js/dependentdropdownfield.js
+++ client/js/dependentdropdownfield.js
@@ -3,31 +3,36 @@ jQuery.entwine("dependentdropdown", function ($) {
 	$(":input.dependent-dropdown").entwine({
 		onmatch: function () {
 			var drop = this;
-			var depends = ($(":input[name=" + drop.data('depends').replace(/[#;&,.+*~':"!^$[\]()=>|\/]/g, "\\$&") + "]"));
+			var depentName = drop.data('depends').replace(/[#;&,.+*~':"!^$[\]()=>|\/]/g, "\\$&");
+			var depends = ($(":input[name=" + depentName + "]"));
 
 			this.parents('.field:first').addClass('dropdown');
 
-			depends.change(function () {
-				if (!this.value) {
-					drop.disable(drop.data('unselected'));
-				} else {
-					drop.disable("Loading...");
-
-					$.get(drop.data('link'), {
-						val: this.value
-					},
-					function (data) {
-						drop.enable();
-
-						if (drop.data('empty') || drop.data('empty') === "") {
-							drop.append($("<option />").val("").text(drop.data('empty')));
-						}
-
-						$.each(data, function () {
-							drop.append($("<option />").val(this.k).text(this.v));
+			// watch change of the dependent field and load new data
+			// since watching on the dependent field seems not working with TreeDropdownField, we watch the whole form changes and determine change for the dependent field
+			this.closest('form').on('change', function(e) {
+				if (e.target.name === depentName) {
+					if (!e.target.value) {
+						drop.disable(drop.data('unselected'));
+					} else {
+						drop.disable("Loading...");
+	
+						$.get(drop.data('link'), {
+							val: e.target.value
+						},
+						function (data) {
+							drop.enable();
+	
+							if (drop.data('empty') || drop.data('empty') === "") {
+								drop.append($("<option />").val("").text(drop.data('empty')));
+							}
+	
+							$.each(data, function () {
+								drop.append($("<option />").val(this.k).text(this.v));
+							});
+							drop.trigger("liszt:updated").trigger("chosen:updated").trigger("change");
 						});
-						drop.trigger("liszt:updated").trigger("chosen:updated").trigger("change");
-					});
+					}
 				}
 			});
 
